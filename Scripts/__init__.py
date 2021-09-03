fe5e96a0094fa36bd02fe19d5db1fccfca6b82d5
Richter <richter122@UKG.BE-MRZ.KLIN>
2021-09-03 09:28:22 +0200

    Added setup.py and surroundings for potential upload to PyPi(with Twine)

---
+++ Scripts/__init__.py
@@ -0,0 +1,6 @@
+# Import all functions that shall be available if you load the package via pip install
+from .buildComp import *
+from .configHandler import *
+from .handleOPT import *
+from .mappingListGen import *
+from .pathExport import *
---
+++ Scripts/__version__.py
@@ -0,0 +1,4 @@
+## 2.3.5-0041 for major release . minor release . patch level or micro release and if used build number
+VERSION = (0, 0, 1)
+
+__version__ = '.'.join(map(str, VERSION))
