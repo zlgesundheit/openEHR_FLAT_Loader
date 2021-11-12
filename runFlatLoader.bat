a7ea7bd3df2a0258004e0e3ec8820574148c2ecc
richter122 <jendrik.richter@med.uni-goettingen.de>
2021-11-12 15:26:35 +0100

    Added Error Message if python.exe was not found on PATH or is not named python.

--- runFlatLoader.bat
+++ runFlatLoader.bat
@@ -1,2 +1,10 @@
-python "%~dp0%\main.py"
-pause
+@echo off
+python "%~dp0%\main.py" /?  2> NUL
+
+IF NOT %ERRORLEVEL%==9009 ECHO "Tool finished find the generated files in 'ManualTasks' or 'Output' Directory"
+
+IF %ERRORLEVEL%==9009 ECHO python.exe was not found on PATH. 
+IF %ERRORLEVEL%==9009 ECHO Add python to PATH or substitute the correct path to python.exe in this .bat-file.
+
+pause > NUL
+
