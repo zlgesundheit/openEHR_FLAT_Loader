1a6ba410af4746fa7a4be69bf44b2f9c0a333f35
Richter <jendrik.richter@med.uni-goettingen.de>
2023-10-19 10:52:13 +0200

    Code Review aqlBuilder

--- Scripts/aqlBuilder.py
+++ Scripts/aqlBuilder.py
@@ -11,11 +11,21 @@ OPTDirPath      = os.path.join(workdir, 'OPTs')
 manualTaskDir   = os.path.join(workdir, 'ETLProcess', 'ManualTasks', config.templateName)
 print(config.templateName)
 
-webTemp = handleOPT.main(config,manualTaskDir,OPTDirPath)
+### User muss Template-Namen angeben (in Config oder TODO lieber abfragen)
+### TODO User startet Step_x.bat  (Vorher in config: serveradresse + authinfo)
+### TODO Dann: Vom angegebenen Server werden alle Templates abgefragt und der Nutzer 
+### kann aus einer Liste auswählen zu welchem template er Daten abfragen will.
+### Eingabe in config speichern oder direkt verwenden
+webTemp = handleOPT.get_webtemplate(config,manualTaskDir)
 
 # Extrahiere Pfade in Array von Pfadobjekten
 web_temp_elmnts = handleWebTemplate.main(webTemp, config.templateName)[1] # TODO: return of andleWebTemplate.main() is array, little bit ugly
 
+### TODO Muss man das nochmal anfassen
+### TODO Hier könnten die bestehenden pfadobjekte um ein "aqlPfad" attribut erweitert werden 
+### -> das müsste IN der handlewebtemplate entsprechend beim auslesen hinzugefügt werden
+### Das hieße, dass man auch nur die AQL-Pfade hat, von Elementen wo Daten drin stehen, z.B. nicht von CLustern, o.ä.
+### TODO Das kann auch hintenraus Filter-Arbeit einsparen -> nochmal abgleichen wie das im openehr2csv code derzeit läuft
 aql_path_values = [d["aqlPath"] for d in web_temp_elmnts]
 
 
@@ -37,5 +47,3 @@ def generate_aql(aql_path_values):
 adjusted_aql_string = generate_aql(aql_path_values)
 resp = util.sendAqlRequest(config.targetAdress, config.targetAuthHeader, "9999999999999", adjusted_aql_string)
 util.storeRespAsCSV(workdir, "compositions_as_csvs", resp, config.templateName+".csv", web_temp_elmnts)
-
-
